dffa69ed66f147793ffd40548fe4706ef09297c4
EmanuelCN <emanuelghub@gmail.com>
2024-10-21 22:09:10 +0300

    Revert "timer: Update code that migrates timers and hrtimers during isolation"
    This reverts commit 5fadc772efa2e7f05d19e610a5e3054acbf37d22.

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -1874,12 +1874,6 @@ static void migrate_hrtimer_list(struct hrtimer_cpu_base *old_base,
 				raw_spin_unlock(&old_base->lock);
 				raw_spin_unlock(&new_base->lock);
 				cpu_relax();
-				/*
-				 * cpu_relax may just be a barrier. Grant the
-				 * run_hrtimer_list code some time to obtain
-				 * the spinlock.
-				 */
-				udelay(1);
 				raw_spin_lock(&new_base->lock);
 				raw_spin_lock_nested(&old_base->lock,
 							SINGLE_DEPTH_NESTING);
