5ddbdc967c2bbde7fc6646fd86956eb95b46bb07
Sultanxda <sultanxda@gmail.com>
2025-08-22 14:06:08 +0300

    firmware: qcom: Remove debugfs dependency from tz-log driver
    Without this driver (specifically, its probe routines), the device crashes
    frequently, so it is needed even when debugfs is disabled.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/firmware/qcom/tz_log.c
+++ drivers/firmware/qcom/tz_log.c
@@ -491,7 +491,7 @@ static int tzdbg_request_encrypted_log(dma_addr_t buf_paddr,
 /*
  * Debugfs data structure and functions
  */
-
+#ifdef CONFIG_DEBUG_FS
 static int _disp_tz_general_stats(void)
 {
 	int len = 0;
@@ -1316,6 +1316,7 @@ const struct file_operations qsee_log_dump_proc_fops = {
 	.open    = qsee_log_dump_procfs_open,
 };
 
+#endif
 
 /*
  * Allocates log buffer from ION, registers the buffer at TZ
@@ -1470,6 +1471,7 @@ static void tzdbg_free_encrypted_log_buf(struct platform_device *pdev)
 static int  tzdbgfs_init(struct platform_device *pdev)
 {
 	int rc = 0;
+#ifdef CONFIG_DEBUG_FS
 	int i;
 	struct dentry           *dent_dir;
 	struct dentry           *dent;
@@ -1514,12 +1516,13 @@ static int  tzdbgfs_init(struct platform_device *pdev)
 	return 0;
 err:
 	debugfs_remove_recursive(dent_dir);
-
+#endif
 	return rc;
 }
 
 static void tzdbgfs_exit(struct platform_device *pdev)
 {
+#ifdef CONFIG_DEBUG_FS
 	struct dentry *dent_dir;
 	dent_dir = platform_get_drvdata(pdev);
 	debugfs_remove_recursive(dent_dir);
@@ -1528,6 +1531,7 @@ static void tzdbgfs_exit(struct platform_device *pdev)
 
 	if (p_qsee_log_dump_handler != NULL)
 		proc_remove(p_qsee_log_dump_handler);
+#endif
 }
 
 static int __update_hypdbg_base(struct platform_device *pdev,
