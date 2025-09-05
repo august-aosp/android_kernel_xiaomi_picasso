5c891ff4dc48451116d2c01293ba538db9b6d8cc
hoang1007 <hoangvuhuy1007@gmail.com>
2025-09-06 01:49:31 +0700

    Revert "power: step-chg-jeita: Add support to tune hysteresis for jeita-fcc-step"
    This reverts commit c5c21901f30957f1a505a56fd340de42dcfd48fa.

--- drivers/power/supply/qcom/step-chg-jeita.c
+++ drivers/power/supply/qcom/step-chg-jeita.c
@@ -262,7 +262,7 @@ static int get_step_chg_jeita_setting_from_profile(struct step_chg_info *chip)
 	u32 max_fv_uv, max_fcc_ma;
 	const char *batt_type_str;
 	const __be32 *handle;
-	int batt_id_ohms, rc, hysteresis[2] = {0};
+	int batt_id_ohms, rc;
 	union power_supply_propval prop = {0, };
 
 	handle = of_get_property(chip->dev->of_node,
