3b389a98c9b765a278a9543d76a4f01a08134509
hoang1007 <hoangvuhuy1007@gmail.com>
2025-09-27 16:33:19 +0700

    include: Remove Xiaomi modifications

--- include/dt-bindings/iio/qcom,spmi-vadc.h
+++ include/dt-bindings/iio/qcom,spmi-vadc.h
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2012-2014,2018-2020 The Linux Foundation. All rights reserved.
- * Copyright (C) 2021 XiaoMi, Inc.
  */
 
 
@@ -324,6 +323,5 @@
 #define ADC_SCALE_HW_CALIB_THERM_100K_PU_PM7	0x11
 #define ADC_SCALE_HW_CALIB_PMIC_THERM_PM7	0x12
 #define ADC_SCALE_BATT_THERM_QRD_215		0x13
-#define ADC_SCALE_HW_CALIB_MAX			0x20
 
 #endif /* _DT_BINDINGS_QCOM_SPMI_VADC_H */
