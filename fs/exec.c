82788ef83edfef971063beb7c5dfb17a5055b001
hoang1007 <hoangvuhuy1007@gmail.com>
2025-11-25 21:06:15 +0700

    Revert "treewide: Backport SUSFS v1.5.11 for k4.19"
    This reverts commit 88836b6906c16c40e6507398318f984c7c59c1a3.

--- fs/exec.c
+++ fs/exec.c
@@ -66,9 +66,6 @@
 #include <linux/uaccess.h>
 #include <asm/mmu_context.h>
 #include <asm/tlb.h>
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-#include <linux/susfs_def.h>
-#endif
 
 #include <trace/events/task.h>
 #include "internal.h"
@@ -1946,11 +1943,6 @@ extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *ar
 			void *envp, int *flags);
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 				 void *argv, void *envp, int *flags);
-
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-extern bool susfs_is_sus_su_hooks_enabled __read_mostly;
-extern bool __ksu_is_allow_uid(uid_t uid);
-#endif
 #endif
 
 static int do_execveat_common(int fd, struct filename *filename,
@@ -1958,22 +1950,11 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
-#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_SUSFS_SUS_SU)
+#ifdef CONFIG_KSU
 	if (unlikely(ksu_execveat_hook))
 		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	else
 		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
-#endif
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-	if (likely(susfs_is_current_proc_umounted())) {
-		goto orig_flow;
-	}
-	if (likely(susfs_is_sus_su_hooks_enabled) &&
-		unlikely(__ksu_is_allow_uid(current_uid().val)))
-	{
-		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
-	}
-orig_flow:
 #endif
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }
